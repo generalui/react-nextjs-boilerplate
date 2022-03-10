ff1a3e4b9e8473a3072a2a142c1eb17a55758ca3
Andy Frey <andyfrey95@gmail.com>
2022-03-10 13:21:57 -0700

    added jsx-a11y default rules to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,12 +5,15 @@ module.exports = {
 		project: './tsconfig.json',
 		extraFileExtensions: ['.json', '.mjs']
 	},
-	plugins: ['@typescript-eslint', 'unused-imports'],
+	plugins: ['@typescript-eslint', 'unused-imports', 'jsx-a11y'],
 	extends: [
 		'next',
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended'
+		'plugin:prettier/recommended',
+		// JSX-A11y Configurations can be found at
+		// https://github.com/jsx-eslint/eslint-plugin-jsx-a11y
+		'plugin:jsx-a11y/recommended'
 	],
 	rules: {
 		// Nextjs rules omited for static rendering
