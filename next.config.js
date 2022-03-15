6879bc55f989d60ea6bfa1feba0b109aef781d8c
Ariel  Reyes <arielreyes@ariel.local>
2022-03-15 15:37:52 -0700

    PWA implemented

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-	reactStrictMode: true,
-	compiler: {
-		styledComponents: true
-	},
-}
+const withPWA = require("next-pwa");
 
 // Refer to bundle analyzer docs if additional
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
 	enabled: process.env.ANALYZE === 'true'
 })
 
+const nextConfig = {
+	reactStrictMode: true,
+	compiler: {
+		styledComponents: true
+	},
+	pwa: {
+		dest: "public"
+	}
+}
 
-module.exports = withBundleAnalyzer(nextConfig)
+module.exports = withBundleAnalyzer(withPWA(nextConfig))
