d04a2ff0dd3d6948aafe82374e435c90ff998336
RicardoFrabasile <rfrabasile@gmail.com>
2022-03-17 17:00:19 -0300

    counter changes

--- src/components/pages/Counter/Counter.tsx
+++ src/components/pages/Counter/Counter.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react'
 import { decrement, increment, reset, useCounter } from 'state/counter'
 import { clearStoredState } from 'utils/persist'
 import { ButtonBody } from 'components/atoms/ButtonBody/ButtonBody'
@@ -9,41 +8,15 @@ import { Default as DefaultTemplate } from 'components/templates'
 
 export const Counter = () => {
 	const { count } = useCounter()
-	const [loading, setLoading] = useState(false)
-
-	useEffect(() => {
-		setTimeout(() => setLoading(false), 1000)
-	}, [count])
 
 	return (
-<<<<<<< HEAD
-		<DefaultTemplate title='Counter Example' loading={loading}>
-			<div>
-				<h1 className={styles.title}>Counter:</h1> <div data-testid="counter">{count}</div>
-			</div>
-			<div className={styles.buttonContainer}>
-				<StyledButton data-testid="decrement" onClick={decrement}>-1</StyledButton>
-				<StyledButton data-testid="reset" onClick={reset}>reset</StyledButton>
-				<StyledButton data-testid="increment" onClick={increment}>+1</StyledButton>
-			</div>
-			<div className={styles.buttonContainer}>
-				<button
-					onClick={() => {
-						reset()
-						clearStoredState()
-					}}
-				>
-					clear local storage
-				</button>
-			</div>
-=======
 		<DefaultTemplate title='Counter Example'>
 			<FlexContainer>
 				<H1>Counter: {count}</H1>
 				<ButtonsContainer>
-					<ButtonBody onClick={decrement}>-1</ButtonBody>
-					<ButtonBody onClick={reset}>reset</ButtonBody>
-					<ButtonBody onClick={increment}>+1</ButtonBody>
+					<ButtonBody data-testid="decrement" onClick={decrement}>-1</ButtonBody>
+					<ButtonBody data-testid="reset" onClick={reset}>reset</ButtonBody>
+					<ButtonBody data-testid="increment" onClick={increment}>+1</ButtonBody>
 				</ButtonsContainer>
 				<ButtonsContainer>
 					<button
@@ -56,7 +29,6 @@ export const Counter = () => {
 					</button>
 				</ButtonsContainer>
 			</FlexContainer>
->>>>>>> main
 		</DefaultTemplate>
 	)
 }
