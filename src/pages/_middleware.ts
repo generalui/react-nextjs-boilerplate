5c1ee6f35bf4263f96c12d618f5a563ab3b3fdf3
cosmovane <vanessa.cnr97@hotmail.com>
2022-05-27 09:36:36 -0700

    🔒️ Exclude /image in protected routes

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -2,7 +2,12 @@ import { withAuth } from 'next-auth/middleware'
 
 export default withAuth({
 	callbacks: {
-		authorized: ({ req, token }) => (token ? true : false)
+		authorized: ({ req, token }) => {
+			if (req.url.includes('/image')) {
+				return true
+			}
+			return token ? true : false
+		}
 	},
 	pages: {
 		signIn: '/auth/signin'
