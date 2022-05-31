9938d374fab647318943971aac6c961a1f4417af
cosmovane <vanessa.cnr97@hotmail.com>
2022-05-31 16:31:24 -0700

    🗃️  Add document to database

--- src/pages/api/documents/index.ts
+++ src/pages/api/documents/index.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { DocumentOptions, upload } from 'utils/api/media'
+import { prisma } from 'utils/api/prisma'
 
 const documentName = 'https://placekitten.com/200/300'
 const options: DocumentOptions = {
@@ -11,12 +12,27 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 	if (req.method === 'POST') {
 		try {
 			const cloudinaryResponse = await upload(documentName, options)
-			console.log('cloudinaryResponse: ', cloudinaryResponse)
+			const { original_filename, secure_url, resource_type } = cloudinaryResponse
+			const document = await prisma.document.create({
+				data: {
+					name: original_filename,
+					url: secure_url,
+					resourceType: resource_type,
+					userId: 'cl3urk98400008nojg2syb26r' // CHANGE THIS ID!!
+				}
+			})
+			console.log('document: ', document)
+			res.status(200).json({ document })
 		} catch (error) {
-			console.log('error: ', error)
 			res.status(400).json({ message: error })
 		}
 	} else {
-		console.log('another request')
+		res.status(400).json({ message: 'That request does not exist' })
 	}
 }
+
+// error codes (pdf, jpg, png)
+// Automatic document options
+// Save in postgres
+// Upload from frontend
+// convert to data buffer or whatever
