dad93488b36c665494bb90efc2740f57e0341f86
cosmovane <vanessa.cnr97@hotmail.com>
2022-06-10 15:14:33 -0700

    ➕ Add flowbite and flowbite react

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,11 +1,24 @@
+import { Modal } from 'flowbite-react'
+import { useState } from 'react'
 import { useText } from 'hooks/useText'
+import { Button } from 'components/common/Button'
 import { CreateStudy } from 'components/partials/CreateStudy'
 import { FileInput } from 'components/partials/FileInput'
 import { PageWrapper } from 'partials/PageWrapper'
 import { Card } from 'common/Card'
 
 export const Home = () => {
+	const [open, setOpen] = useState<boolean>(false)
+
 	const { t } = useText('home')
+
+	const handleOnClick = () => {
+		setOpen(!open)
+	}
+
+	const handleOnClose = () => {
+		setOpen(false)
+	}
 
 	return (
 		<PageWrapper title={t('title')}>
@@ -19,6 +32,15 @@ export const Home = () => {
 						<h4>{t('welcomeHeading.description')}</h4>
 					</Card>
 					<CreateStudy />
+					<div>
+						<Button onClick={handleOnClick}>Toggle modal</Button>
+						<Modal show={open} onClose={handleOnClose}>
+							<Modal.Header>Terms of Service</Modal.Header>
+							<Modal.Body>
+								<h1>Modal 1</h1>
+							</Modal.Body>
+						</Modal>
+					</div>
 				</div>
 				<div className='w-full col-span-4 lg:col-span-1'>
 					<div className='grid grid-cols-1 gap-6'>
