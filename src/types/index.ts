2f17e7b6bb5ef681771285901c7a285ec37cd7a6
cosmovane <vanessa.cnr97@hotmail.com>
2022-06-16 14:07:00 -0700

    🏷️ Remove duplicated exports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,8 +1,6 @@
 // Automated exports
 export * from './Sizes'
 export * from './ReqStatus'
-// Automated exports
-export * from './Sizes'
 export * from './OnChangeValue'
 export * from './ApiRequestWithFile'
 export * from './CommonProps'
