beba3679008d76af175203876f7870a44da46bdd
Jase Pellerin <jasepellerin@gmail.com>
2022-06-22 10:38:17 -0700

    :bug: Fix dropdown button styles and duplicated type

--- src/components/common/DropDown/DropDownItem/DropDownItem.tsx
+++ src/components/common/DropDown/DropDownItem/DropDownItem.tsx
@@ -11,7 +11,10 @@ export const DropDownItem = ({
 	return (
 		<Button
 			onClick={onClick}
-			className={cn('hover:bg-gray-100 py-2 w-full rounded-xl', className)}
+			className={cn(
+				'bg-transparent hover:bg-gray-100 text-black font-normal py-2 w-full rounded-xl',
+				className
+			)}
 			testId={testId}
 		>
 			{children}
