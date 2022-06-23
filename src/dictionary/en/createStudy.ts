a810fc4ba07ca2bd55df99e1cdce272bb1a1a59a
Devan Huapaya <devan@genui.com>
2022-06-22 17:06:06 -0700

    🚨 Add react/jsx-no-literals and resolve errors

--- src/dictionary/en/createStudy.ts
+++ src/dictionary/en/createStudy.ts
@@ -1,6 +1,25 @@
 const createStudy = {
 	title: {
 		message: 'Create Study'
+	},
+	fields: {
+		coordinator: {
+			message: 'Coordinator'
+		},
+		endDate: {
+			message: 'End Date'
+		},
+		description: {
+			message: 'Study Description'
+		}
+	},
+	buttons: {
+		submit: {
+			message: 'Create Study'
+		},
+		cancel: {
+			message: 'Cancel'
+		}
 	}
 }
 
