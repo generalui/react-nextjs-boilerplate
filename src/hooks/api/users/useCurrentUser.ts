26dc7adc724b6c12a1a4257c37061e85d8ec8cbb
Devan Huapaya <devan@genui.com>
2022-07-01 16:15:54 -0700

    ✨ Add logrocket

--- src/hooks/api/users/useCurrentUser.ts
+++ src/hooks/api/users/useCurrentUser.ts
@@ -1,4 +1,6 @@
 import { User } from '@prisma/client'
+import LogRocket from 'logrocket'
+import { useEffect } from 'react'
 import { UseQueryResult, useQuery } from 'react-query'
 import { getCurrentUser } from 'utils/api/getCurrentUser'
 
@@ -6,5 +8,15 @@ export const useCurrentUser = (): Omit<UseQueryResult<Awaited<User>, unknown>, '
 	currentUser?: User
 } => {
 	const { data: currentUser, ...query } = useQuery('current-user', getCurrentUser)
+
+	useEffect(() => {
+		if (process.env.NEXT_PUBLIC_URL === 'staging' && currentUser)
+			LogRocket.identify(currentUser.id, {
+				name: currentUser.name || '',
+				email: currentUser.email || ''
+
+				// Add your own custom user variables here, ie:
+			})
+	}, [currentUser])
 	return { currentUser, ...query }
 }
