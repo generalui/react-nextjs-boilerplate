654ad620aac5a380b40a5d149c054da204cc9165
cosmovane <vanessa.cnr97@hotmail.com>
2022-07-05 14:30:38 -0700

    💬 Add useText to StudyForm placeholders

--- src/components/partials/Form/StudyForm.tsx
+++ src/components/partials/Form/StudyForm.tsx
@@ -26,25 +26,30 @@ export const StudyForm = ({
 					<div className='grid grid-cols-3 gap-2'>
 						<ImageInput name='image' className='row-span-3' />
 						<div className='col-span-2 row-span-2'>
-							<Input name='title' placeholder='Title' type='textarea' />
+							<Input name='title' placeholder={t('placeholders.title')} type='textarea' />
 						</div>
 						<div>
 							<label className='text-xs text-gray-500' htmlFor='coordinator'>
 								{t('fields.coordinator')}
 							</label>
-							<Input name='coordinator' type='text' placeholder='Coordinator' />
+							<Input name='coordinator' type='text' placeholder={t('placeholders.coordinator')} />
 						</div>
 						<div>
 							<label className='text-xs text-gray-500' htmlFor='endDate'>
 								{t('fields.endDate')}
 							</label>
-							<Input name='endDate' type='date' placeholder='Select End Date' />
+							<Input name='endDate' type='date' placeholder={t('placeholders.endDate')} />
 						</div>
 						<label className='text-xs text-gray-500' htmlFor='description'>
 							{t('fields.description')}
 						</label>
 						<div className='col-span-3'>
-							<Input name='description' placeholder='Description' type='textarea' rows={5} />
+							<Input
+								name='description'
+								placeholder={t('placeholders.description')}
+								type='textarea'
+								rows={5}
+							/>
 						</div>
 						<div className='col-span-3 flex gap-2'>
 							<SubmitButton isLoading={isLoading} disableOnLoading>
