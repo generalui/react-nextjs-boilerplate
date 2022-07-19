56c60fb04f62f9f1138a28cdd723911756ce7098
cosmovane <vanessa.cnr97@hotmail.com>
2022-07-19 11:29:58 -0700

    💄 Style ModalFooterButtons

--- src/components/common/ModalFooterButtons/ModalFooterButtons.tsx
+++ src/components/common/ModalFooterButtons/ModalFooterButtons.tsx
@@ -1,6 +1,7 @@
 import { useModal } from 'hooks/useModal'
 import { useText } from 'hooks/useText'
 import { Button } from 'common/Button'
+import { ModalFooter } from 'common/ModalFooter/ModalFooter'
 import { ModalFooterButtonsProps } from './ModalFooterButtons.types'
 
 export const ModalFooterButtons = ({
@@ -15,10 +16,12 @@ export const ModalFooterButtons = ({
 
 	return (
 		<div className={className} data-testid={testId}>
-			<Button onClick={actionButtonHandler}>{actionButtonLabel}</Button>
-			<Button v='secondary' onClick={close}>
-				{t('cancel')}
-			</Button>
+			<ModalFooter className='mt-4'>
+				<Button onClick={actionButtonHandler}>{actionButtonLabel}</Button>
+				<Button v='secondary' onClick={close}>
+					{t('cancel')}
+				</Button>
+			</ModalFooter>
 		</div>
 	)
 }
