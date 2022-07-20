a1115b9e821dcc2a9d50f6258b56f2154451dbf2
Jase Pellerin <jasepellerin@gmail.com>
2022-07-20 15:11:49 -0700

    ✨ Add data vault aggregated data on home page

--- src/pages/api/aggregated-study-data.ts
+++ src/pages/api/aggregated-study-data.ts
@@ -9,21 +9,29 @@ const apiRoute = connect()
 // Get a aggregated study data
 apiRoute.get(async (req: NextApiRequest, res: NextApiResponse) => {
 	const studyQuery = async () => {
-		const [totalStudies, totalDocuments] = await prisma.$transaction([
+		const [totalStudies, totalDataVaultElements, totalDocuments] = await prisma.$transaction([
 			prisma.study.count(),
 			prisma.document.aggregate({
 				_count: {
+					_all: true
+				},
+				where: {
+					NOT: { dataVault: null }
+				}
+			}),
+			prisma.document.aggregate({
+				_count: {
 					studyId: true
+				},
+				where: {
+					dataVault: null
 				}
 			})
 		])
 
-		// TODO: get data vault elements
-		const totalDataVaultElements = 0
-
 		return {
 			totalStudies,
-			totalDataVaultElements,
+			totalDataVaultElements: totalDataVaultElements._count._all,
 			totalDocuments: totalDocuments._count.studyId
 		}
 	}
