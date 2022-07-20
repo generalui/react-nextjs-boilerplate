e1a336c8dcadc745e18d517f2f3ca01f0f71bfd2
Jase Pellerin <jasepellerin@gmail.com>
2022-07-20 15:12:45 -0700

    ✅ Fix tests

--- src/components/partials/DataVaultList/DataVaultList.spec.tsx
+++ src/components/partials/DataVaultList/DataVaultList.spec.tsx
@@ -2,12 +2,13 @@
  * Test file for DataVaultList
  */
 import { render, screen } from '@testing-library/react'
+import '__mocks__/index'
 import React from 'react'
 import { DataVaultList } from './index'
 
 describe('DataVaultList Component', () => {
 	it('renders on the page', () => {
-		render(<DataVaultList />)
+		render(<DataVaultList studyId={''} />)
 
 		const component = screen.getByTestId('DataVaultList')
 
