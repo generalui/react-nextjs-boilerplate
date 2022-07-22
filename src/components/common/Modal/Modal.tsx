4150289fa0c604f118514581d55256523266b079
cosmovane <vanessa.cnr97@hotmail.com>
2022-07-22 16:43:58 -0700

    📱 Enhance modal responsiveness

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -22,17 +22,17 @@ export const Modal = memo(function Modal({
 			{/* Background Container */}
 			<div
 				id='popup-modal'
-				className=' overflow-y-auto overflow-x-hidden fixed top-0 right-0 bottom-0 left-0 z-50 md:inset-0 h-full md:h-full items-center justify-center flex bg-gray-900 bg-opacity-50 dark:bg-opacity-80'
+				className=' overflow-y-auto overflow-x-hidden fixed top-0 right-0 bottom-0 left-0 z-50 items-center justify-center flex bg-gray-900 bg-opacity-50 dark:bg-opacity-80 h-full'
 			>
 				{/* Modal Container */}
 				<div
 					className={cn(
-						'relative bg-white sm:bg-transparent sm:p-4 w-full h-full md:h-auto md:m-16',
+						'relative bg-white w-full h-full sm:bg-transparent sm:mt-8 max-h-screen box-border sm:h-auto',
 						`max-w-${size}`
 					)}
 				>
 					{/* Modal */}
-					<div className='relative p-3 bg-white sm:rounded-lg sm:shadow dark:bg-gray-700 '>
+					<div className='relative p-3 bg-white sm:rounded-lg md:shadow dark:bg-gray-700 '>
 						{/* Modal Header */}
 						{title &&
 							(typeof title === 'string' ? (
