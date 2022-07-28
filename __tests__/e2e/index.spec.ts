e5a73420f41ebd939dc814351caf044a7848e984
Jase Pellerin <jasepellerin@gmail.com>
2022-07-28 15:11:09 -0700

    ✅ Add simple playwright setup that works locally

--- __tests__/e2e/index.spec.ts
+++ __tests__/e2e/index.spec.ts
@@ -1,9 +1,25 @@
 // tests/index.spec.ts
 import { expect } from '@playwright/test'
+import { users } from 'prisma/seedData'
 import test from './next-fixture'
 
-test('The app loads', async ({ page, port }) => {
-	await page.goto(`http://localhost:${port}/`)
-	const name = await page.innerText('h1')
-	expect(name).toBe('Case Studies')
+test('The sign in page loads', async ({ page }) => {
+	await page.goto(`/`)
+	await expect(page).toHaveTitle('Sign in')
+})
+
+test('Test user can sign in', async ({ page }) => {
+	await page.goto('/')
+
+	const testUser = users[0]
+
+	await page.locator('[placeholder="Email"]').click()
+	await page.locator('[placeholder="Email"]').fill(testUser.email)
+
+	await page.locator('[placeholder="Password"]').click()
+	await page.locator('[placeholder="Password"]').fill(testUser?.password || '')
+
+	await page.locator('[data-testid="SubmitButton"]').click()
+
+	await expect(page).toHaveTitle('Home')
 })
