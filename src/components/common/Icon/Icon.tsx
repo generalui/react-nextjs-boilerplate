8aac8e3d788fab8b4d508d11fbc79abed01eed25
Jase Pellerin <jasepellerin@gmail.com>
2022-08-09 11:42:07 -0700

    ♻️  Clean up svgs, add svgr, update Icon component to allow using internal icons

--- src/components/common/Icon/Icon.tsx
+++ src/components/common/Icon/Icon.tsx
@@ -1,17 +1,32 @@
 import * as OutlinedIcons from '@heroicons/react/outline'
 import * as SolidIcons from '@heroicons/react/solid'
 import cn from 'classnames'
+import * as InternalIcons from 'public/icons'
 import { iconVariants } from 'common/Icon/variants'
 import { IconProps } from './Icon.types'
 
-export const Icon = ({ className, icon, outlined, size = 'md', testId = 'Icon' }: IconProps) => {
-	const Icon = outlined ? OutlinedIcons[icon] : SolidIcons[icon]
+const isHiIcon = (icon: string): icon is keyof typeof SolidIcons => {
+	return icon in SolidIcons
+}
 
-	return (
+export const Icon = ({ className, icon, outlined, size = 'md', testId = 'Icon' }: IconProps) => {
+	let Icon = null
+
+	if (isHiIcon(icon)) {
+		Icon = outlined ? OutlinedIcons[icon] : SolidIcons[icon]
+	} else {
+		Icon = InternalIcons[icon]
+	}
+
+	return Icon ? (
 		<Icon
-			className={cn('flex-shrink-0', iconVariants[size], className)}
+			className={cn(
+				'flex-shrink-0 flex items-center justify-center',
+				iconVariants[size],
+				className
+			)}
 			aria-hidden='true'
 			data-testid={testId}
 		/>
-	)
+	) : null
 }
--- src/components/common/Icon/Icon.types.ts
+++ src/components/common/Icon/Icon.types.ts
@@ -1,9 +1,10 @@
 import * as HIcons from '@heroicons/react/solid'
+import * as InternalIcons from 'public/icons'
 import { CommonProps } from 'types/CommonProps'
 import { iconVariants } from 'common/Icon/variants'
 
 export interface IconProps extends CommonProps {
-	icon: keyof typeof HIcons
+	icon: keyof typeof HIcons | keyof typeof InternalIcons
 	outlined?: boolean
 	size?: keyof typeof iconVariants
 }
