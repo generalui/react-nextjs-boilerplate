45f87d9e551a88de233661364351bf2647b47ae0
Jase Pellerin <jasepellerin@gmail.com>
2022-08-25 10:50:37 -0700

    ✨ Improve study image edit icon visibility on dark images

--- src/components/common/ImageInput/variants.ts
+++ src/components/common/ImageInput/variants.ts
@@ -1,25 +1,26 @@
 const baseContainerStyles = 'relative rounded-lg overflow-hidden flex flex-col grow-0 items-center'
 const basePreviewStyles = 'block w-full h-full bg-cover bg-center'
 const baseDisabledStyles = 'cursor-default'
-const baseIconStyles = 'h-5 w-5 absolute bottom-3.5'
+const baseIconWrapperStyles =
+	'absolute bottom-2 p-2 bg-white bg-opacity-75 rounded-full flex align-items-center justify-center'
 
 const defaultContainerStyles = `${baseContainerStyles} border border-gray-400 focus:border-2 focus:border-primary focus:outline-2  focus:outline-gray-400  h-40 w-40 lg:h-[200px] lg:w-[200px]`
 const defaultPreviewStyles = `${basePreviewStyles} absolute left-0 top-0`
-const defaultIconStyles = `${baseIconStyles} right-3.5`
+const defaultIconWrapperStyles = `${baseIconWrapperStyles} right-2`
 
 const roundedContainerStyles = `${baseContainerStyles} items-center w-36 h-36 rounded-full lg:w-40 lg:h-40`
-const roundedIconStyles = `${baseIconStyles} right-8`
+const roundedIconWrapperStyles = `${baseIconWrapperStyles} right-7`
 
 export const imageInputVariants = {
 	default: {
 		container: defaultContainerStyles,
 		preview: defaultPreviewStyles,
-		icon: defaultIconStyles
+		iconWrapper: defaultIconWrapperStyles
 	},
 	rounded: {
 		container: roundedContainerStyles,
 		preview: basePreviewStyles,
-		icon: roundedIconStyles
+		iconWrapper: roundedIconWrapperStyles
 	}
 }
 
@@ -27,11 +28,11 @@ export const disabledVariants = {
 	defaultDisabled: {
 		container: `${defaultContainerStyles} ${baseDisabledStyles}`,
 		preview: defaultPreviewStyles,
-		icon: defaultIconStyles
+		iconWrapper: defaultIconWrapperStyles
 	},
 	roundedDisabled: {
 		container: `${roundedContainerStyles} ${baseDisabledStyles}`,
 		preview: basePreviewStyles,
-		icon: roundedIconStyles
+		iconWrapper: roundedIconWrapperStyles
 	}
 }
