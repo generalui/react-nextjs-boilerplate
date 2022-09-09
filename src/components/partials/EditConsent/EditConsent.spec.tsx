5bcc2952f8b5147607874b50901855ffdca6e176
cosmovane <vanessa.cnr97@hotmail.com>
2022-09-09 15:59:21 -0700

    🧪 Fix EditConsent test

--- src/components/partials/EditConsent/EditConsent.spec.tsx
+++ src/components/partials/EditConsent/EditConsent.spec.tsx
@@ -2,12 +2,13 @@
  * Test file for EditConsent
  */
 import { render, screen } from '@testing-library/react'
+import '__mocks__/index'
 import React from 'react'
 import { EditConsent } from './index'
 
 describe('EditConsent Component', () => {
 	it('renders on the page', () => {
-		render(<EditConsent />)
+		render(<EditConsent modalName='test' />)
 
 		const component = screen.getByTestId('EditConsent')
 
