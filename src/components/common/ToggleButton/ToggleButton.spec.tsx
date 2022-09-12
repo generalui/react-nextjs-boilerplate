ab43a74edb538b3eee1ff5e1be2200711c52bb0d
cosmovane <vanessa.cnr97@hotmail.com>
2022-09-12 16:34:39 -0700

    ✅ Fix ToggleButton test

--- src/components/common/ToggleButton/ToggleButton.spec.tsx
+++ src/components/common/ToggleButton/ToggleButton.spec.tsx
@@ -3,11 +3,19 @@
  */
 import { render, screen } from '@testing-library/react'
 import React from 'react'
+import { Form } from 'partials/Form'
 import { ToggleButton } from './index'
 
 describe('ToggleButton Component', () => {
 	it('renders on the page', () => {
-		render(<ToggleButton />)
+		render(
+			<Form
+				onSubmit={() => {
+					return
+				}}
+				render={() => <ToggleButton activeLabel='Active' inactiveLabel='Inactive' name='test' />}
+			/>
+		)
 
 		const component = screen.getByTestId('ToggleButton')
 
