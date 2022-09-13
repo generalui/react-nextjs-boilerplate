1d2a949b9f33759331474afea3b03d2b3567df70
cosmovane <vanessa.cnr97@hotmail.com>
2022-09-13 11:58:43 -0700

    🐛 Add correct color to ProfileDropDown hover

--- src/components/common/Button/variants.ts
+++ src/components/common/Button/variants.ts
@@ -1,5 +1,5 @@
 const defaultStyles =
-	'text-button-text-primary bg-primary hover:bg-primary-hover focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md px-4 py-2.5 focus:outline-none flex items-center gap-1'
+	'text-button-text-primary bg-primary focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-md px-4 py-2.5 focus:outline-none flex items-center gap-1'
 const disabledBaseStyles = 'bg-gray-200 hover:bg-gray-200 focus:bg-gray-500'
 const disabledWithBorderStyles = 'border border-gray-200'
 const defaultToggleStyles =
@@ -8,11 +8,11 @@ const selectedToggleStyles =
 	'py-2 px-4 text-sm font-medium bg-primary text-white hover:bg-primary-hover focus:z-10'
 
 export const buttonVariants = {
-	default: `${defaultStyles} border border-primary hover:border-primary-hover`,
+	default: `${defaultStyles} border border-primary hover:border-primary-hover hover:bg-primary-hover`,
 	danger: `${defaultStyles} border border-danger text-button-text-primary bg-danger hover:bg-danger-hover hover:border-danger-hover`,
 	secondary: `${defaultStyles} border border-primary text-button-text-secondary bg-secondary hover:bg-secondary-hover hover:border-secondary-hover`,
-	sm: `${defaultStyles} text-sm px-4 py-2`,
-	xs: `${defaultStyles} text-sm px-3 py-1`,
+	sm: `${defaultStyles} text-sm px-4 py-2 hover:border-primary-hover hover:bg-primary-hover`,
+	xs: `${defaultStyles} text-sm px-3 py-1 hover:border-primary-hover hover:bg-primary-hover`,
 	transparent: `${defaultStyles} bg-transparent hover:bg-gray-100 text-black font-normal py-2`,
 	toggleLeft: `${defaultToggleStyles} rounded-l-lg`,
 	toggleRight: `${defaultToggleStyles} rounded-r-md`,
@@ -21,7 +21,7 @@ export const buttonVariants = {
 }
 
 export const disabledVariants = {
-	defaultDisabled: `${defaultStyles} ${disabledWithBorderStyles} ${disabledBaseStyles}`,
+	defaultDisabled: `${defaultStyles} ${disabledWithBorderStyles} ${disabledBaseStyles} border border-primary hover:border-primary-hover hover:bg-primary-hover`,
 	dangerDisabled: `${defaultStyles} border border-danger text-button-text-secondary bg-secondary hover:bg-secondary-hover hover:border-secondary-hover`,
 	secondaryDisabled: `${defaultStyles} ${disabledWithBorderStyles} text-button-text-secondary border-gray-600 hover:bg-gray-600 focus:bg-gray-600`,
 	smDisabled: `${buttonVariants.sm}  ${disabledBaseStyles}`,
