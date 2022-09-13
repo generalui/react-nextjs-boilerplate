e401fb4489d5a5dda4c35a24b5fe746d7edbd808
Devan Huapaya <devan@genui.com>
2022-09-13 14:59:50 -0700

    ✨ Add consent to schema

--- src/pages/api/studies/[studyId]/add-participants.ts
+++ src/pages/api/studies/[studyId]/add-participants.ts
@@ -103,7 +103,7 @@ apiRoute.put(async (req: ApiRequestWithFile, res: NextApiResponse) => {
 							data: {
 								participants: {
 									connectOrCreate: {
-										create: { participant: { connect: { id: participantUser.id } } },
+										create: { participant: { connect: { id: participantUser.id } }, consent: {} },
 										where: {
 											studyId_participantId: { studyId, participantId: participantUser.id }
 										}
