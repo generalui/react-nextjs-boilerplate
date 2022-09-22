1eac96029fa608e9de3ecb3c8caf8435247f0909
cosmovane <vanessa.cnr97@hotmail.com>
2022-09-22 16:41:38 -0700

    ⚰️ Remove keepPreviousData from useQueryBuilder

--- src/hooks/api/queryBuilder/useQueryBuilder.ts
+++ src/hooks/api/queryBuilder/useQueryBuilder.ts
@@ -3,9 +3,7 @@ import { ApiQueryResults, QueryBuilderParams } from 'types/QueryBuilder'
 import { getQueryBuilderResults } from 'utils/requests/queryBuilder'
 
 export const useQueryBuilder = (params: QueryBuilderParams) => {
-	const query = useQuery<ApiQueryResults>(['query-builder'], () => getQueryBuilderResults(params), {
-		keepPreviousData: true
-	})
+	const query = useQuery<ApiQueryResults>(['query-builder'], () => getQueryBuilderResults(params))
 
 	return query
 }
