61088675b7dcba1108bb3f65ad6cb8fdc25292f5
Devan Huapaya <devan@genui.com>
2022-11-09 13:04:14 -0800

    🎨 Minor style update

--- src/components/partials/QueryBuilder/FiltersHeader/FiltersHeader.tsx
+++ src/components/partials/QueryBuilder/FiltersHeader/FiltersHeader.tsx
@@ -7,23 +7,23 @@ export const FiltersHeader = ({ className, testId = 'FiltersHeader' }: FiltersHe
 
 	return (
 		<div className={className} data-testid={testId}>
-			<div className='grid grid-cols-8 gap-4 items-center pt-4 pl-7'>
-				<div className='flex flex-col gap-3 col-span-7 md:col-span-1'>
+			<div className='grid grid-cols-12 gap-4 items-center'>
+				<div className='flex flex-col gap-3 col-span-12 md:col-span-2'>
 					<Text size='xs' className='text-gray-500 font-semibold'>
 						{t('filterType')}
 					</Text>
 				</div>
-				<div className='flex flex-col gap-3 col-span-7 md:col-span-2'>
+				<div className='flex flex-col gap-3 col-span-12 md:col-span-3'>
 					<Text size='xs' className='text-gray-500 font-semibold'>
 						{t('fields')}
 					</Text>
 				</div>
-				<div className='flex flex-col gap-3 col-span-7 md:col-span-2'>
+				<div className='flex flex-col gap-3 col-span-12 md:col-span-3'>
 					<Text size='xs' className='text-gray-500 font-semibold'>
 						{t('condition')}
 					</Text>
 				</div>
-				<div className='flex flex-col gap-3 col-span-7 md:col-span-2'>
+				<div className='flex flex-col gap-3 col-span-12 md:col-span-3'>
 					<Text size='xs' className='text-gray-500 font-semibold'>
 						{t('value')}
 					</Text>
