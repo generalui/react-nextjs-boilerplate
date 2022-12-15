5cd15010c76e7a7b6b72fda6258a4380dc9c0edb
Vanessa Nunez <vanessa.cnr97@hotmail.com>
2022-12-15 16:28:52 -0600

    🚩 Add email sending flag

--- src/utils/api/sendgrid.ts
+++ src/utils/api/sendgrid.ts
@@ -3,13 +3,17 @@ import sgMail, { MailDataRequired, ResponseError } from '@sendgrid/mail'
 sgMail.setApiKey(process.env.SENDGRID_API_KEY as string)
 
 export default async function sendEmailNotification(msg: MailDataRequired) {
-	return await sgMail
-		.send(msg)
-		.then((response) => {
-			return response[0]
-		})
-		.catch((error) => {
-			console.log('sendEmailNotification ~ error', error)
-			return error as ResponseError
-		})
+	if (process.env.EMAIL_SENDING_IS_ENABLED === 'false')
+		return { statusCode: 200, message: 'Email sending is disabled' }
+	else {
+		return await sgMail
+			.send(msg)
+			.then((response) => {
+				return response[0]
+			})
+			.catch((error) => {
+				console.log('sendEmailNotification ~ error', error)
+				return error as ResponseError
+			})
+	}
 }
