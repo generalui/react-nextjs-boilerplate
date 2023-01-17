e1e974402b1f24a7fbe4b65251f77eabd7fd92c9
Devan Huapaya <devan@genui.com>
2023-01-17 14:51:29 -0800

    Update client config

--- client.config.js
+++ client.config.js
@@ -1,19 +1,18 @@
 module.exports = {
-	name: 'Native BioData Consortium',
-	headerLogo: '/images/NBDC_logo_full.svg',
-	signInImage: '/images/NBDC_logo_full.svg',
-	favicon: '/images/nbdc_favicon.svg',
-	backgroundColor: '#f1ece9',
-	defaultMetaTitle: 'Native BioData',
+	name: 'GenUI',
+	headerLogo: '/images/genui_logo.svg',
+	signInImage: '/images/genui_logo.svg',
+	favicon: '/images/genui_logo.svg',
 	about: {
-		title: 'Stewardship of Native BioData',
-		image: 'https://nativebio.org/wp-content/uploads/2020/12/Guthrie_profile_450.png',
+		title: 'GenUI React Development Kit',
+		image: '/images/fist_bump.jpg',
 		imageAlt: 'NBDC steward',
 		description:
-			'The NBDC platform puts Tribes, and Participants of research studies, in control of their own data. The Consent Dashboard facilitates changes to consent on a study-by-study basis, and includes helpful information with each type of collected data.',
-		link: 'https://nativebio.org/',
-		linkLabel: 'Visit nativebio.org'
+			'This project template was engineered to get your GenUI project up and running in no time!',
+		link: 'https://github.com/generalui/react-nextjs-boilerplate',
+		linkLabel: 'Read the readme'
 	},
+	backgroundColor: 'rgba(0, 0, 0, 0.039)',
 	tailwindTheme: {
 		extend: {
 			fontFamily: {
@@ -27,28 +26,29 @@ module.exports = {
 				}
 			},
 			colors: {
-				primary: '#0093D8',
-				'primary-hover': '#0773b0',
+				primary: '#37f49e',
+				'primary-hover': '#27e48e',
 				'primary-light': '',
 				'primary-light-hover': '#27e48e',
 				'primary-dark': '#222222',
 				'primary-dark-hover': '#27e48e',
-				'button-text-primary': '#FFFFFF',
-				'button-text-secondary': '#0093D8',
-				secondary: '#D5F2FF',
-				'secondary-hover': '#C7E9FF',
+				'button-text-primary': '#222222',
+				'button-text-secondary': '#FFFFFF',
+				secondary: '#222',
+				'secondary-hover': '#222',
 				'secondary-dark': '#111',
 				'secondary-text-dark': '#222222',
-				'accent-1': 'rgb(49, 196, 141)',
-				'accent-2': 'rgb(118, 169, 250)',
-				'accent-3': 'rgb(249, 128, 128)',
-				success: 'rgb(49, 196, 141)',
+				'accent-1': 'cyan',
+				'accent-2': 'magenta',
+				'accent-3': 'orange',
+				success: '#37f49e',
 				warning: 'rgb(253, 186, 140)',
 				danger: '#e74c3c',
-				'danger-hover': '#d73c2c',
 				info: 'rgb(49, 196, 141)',
-				muted: 'rgb(209, 213, 219)',
-				'blue-700': '#0773b0'
+				muted: 'rgb(209, 213, 219)'
+				// You can also edit tail wind colors here e.g. `'blue-600': 'green'
+				// This is not recommended.
+				// Use primary, secondary, or add a relevant named color that can easily changed.
 			}
 		}
 	}
